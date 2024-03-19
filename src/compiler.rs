871a79dc15e5937817142c3372a4ee77b56ed7a0
Héctor Ramón Jiménez <hector@hecrj.dev>
2024-03-19 20:01:25 +0100

    Create `compiler` module and organize logic

---
+++ src/compiler.rs
@@ -0,0 +1,261 @@
+use anyhow::Error;
+
+use std::collections::BTreeSet;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Compiler {
+    build: PathBuf,
+    src: PathBuf,
+    artifacts: PathBuf,
+    hash: u64,
+}
+
+impl Compiler {
+    pub fn set_up(root: impl AsRef<Path>, reference: Reference) -> Result<Self, Error> {
+        let build = root.as_ref().join("target").join("icebergs");
+        fs::create_dir_all(&build)?;
+
+        let src = build.join("src");
+        fs::create_dir_all(&src)?;
+
+        let artifacts = build.join("target").join("mdbook");
+        fs::create_dir_all(&src)?;
+
+        let cargo_config = CARGO_TOML.replace(
+            "{{ GIT_REFERENCE }}",
+            &match reference {
+                Reference::Revision(revision) => format!("rev = \"{revision}\""),
+                Reference::Branch(branch) => format!("branch = \"{branch}\""),
+                Reference::Tag(tag) => format!("tag = \"{tag}\""),
+            },
+        );
+        fs::write(build.join("Cargo.toml"), cargo_config.trim_start())?;
+
+        let hash = {
+            use std::hash::{DefaultHasher, Hash, Hasher};
+
+            let mut hasher = DefaultHasher::new();
+            cargo_config.hash(&mut hasher);
+            hasher.finish()
+        };
+
+        Ok(Self {
+            build,
+            src,
+            artifacts,
+            hash,
+        })
+    }
+
+    pub fn compile(&self, code: &str) -> Result<Iceberg, Error> {
+        use itertools::Itertools;
+        use sha2::{Digest, Sha256};
+
+        let code = code
+            .lines()
+            .map(|line| line.strip_prefix("# ").unwrap_or(line))
+            .join("\n");
+
+        let hash = Hash(
+            Sha256::digest(&code)
+                .into_iter()
+                .map(|byte| format!("{byte:x}"))
+                .join(""),
+        );
+
+        let artifact_dir = self.artifacts.join(hash.as_str());
+
+        if artifact_dir.exists() {
+            return Ok(Iceberg { hash });
+        }
+
+        fs::write(self.src.join("main.rs"), code)?;
+
+        let compilation = process::Command::new("cargo")
+            .args(["build", "--release", "--target", "wasm32-unknown-unknown"])
+            .env("RUSTFLAGS", "")
+            .current_dir(&self.build)
+            .stdout(process::Stdio::piped())
+            .spawn()?;
+
+        std::io::copy(
+            &mut std::io::BufReader::new(compilation.stdout.expect("Open compilation output")),
+            &mut std::io::stderr(),
+        )?;
+
+        process::Command::new("wasm-bindgen")
+            .args([
+                "--target",
+                "web",
+                "--no-typescript",
+                "--out-dir",
+                artifact_dir.to_str().expect("valid artifact path"),
+                "target/wasm32-unknown-unknown/release/iceberg.wasm",
+            ])
+            .current_dir(&self.build)
+            .spawn()?
+            .wait()?;
+
+        Ok(Iceberg { hash })
+    }
+
+    pub fn retain(&self, icebergs: &BTreeSet<Iceberg>) -> Result<(), Error> {
+        clean_dir(&self.artifacts, &icebergs)?;
+
+        Ok(())
+    }
+
+    pub fn release<'a>(
+        &self,
+        icebergs: &BTreeSet<Iceberg>,
+        target: impl AsRef<Path>,
+    ) -> Result<(), Error> {
+        let target = target.as_ref();
+        clean_dir(target, &icebergs)?;
+
+        for iceberg in icebergs {
+            let output = target.join(iceberg.hash.as_str());
+
+            if !output.exists() {
+                let artifact = self.artifacts.join(iceberg.hash.as_str());
+
+                copy_dir_all(artifact, output)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Iceberg {
+    hash: Hash,
+}
+
+impl Iceberg {
+    pub fn embed(&self) -> String {
+        EMBEDDING.replace("{{ HASH }}", self.hash.as_str())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Reference {
+    Revision(String),
+    Branch(String),
+    Tag(String),
+}
+
+impl Reference {
+    pub fn parse(table: &toml::value::Table) -> Result<Self, Error> {
+        if let Some(toml::Value::String(revision)) = table.get("rev") {
+            return Ok(Self::Revision(revision.clone()));
+        }
+
+        if let Some(toml::Value::String(branch)) = table.get("branch") {
+            return Ok(Self::Branch(branch.clone()));
+        }
+
+        if let Some(toml::Value::String(tag)) = table.get("tag") {
+            return Ok(Self::Tag(tag.clone()));
+        }
+
+        Err(Error::msg(
+            "No Git reference found for `iced` in the preprocessor configuration. \
+            Please, specify a `rev`, `branch` or `tag`.",
+        ))
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Hash(String);
+
+impl Hash {
+    fn as_str(&self) -> &str {
+        &self.0
+    }
+}
+
+fn clean_dir(dir: impl AsRef<Path>, icebergs: &BTreeSet<Iceberg>) -> Result<(), Error> {
+    let deleted = fs::read_dir(dir)?
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| {
+            !icebergs
+                .iter()
+                .any(|iceberg| iceberg.hash.as_str() == entry.file_name())
+        });
+
+    for entry in deleted {
+        fs::remove_dir_all(entry.path())?;
+    }
+
+    Ok(())
+}
+
+fn copy_dir_all(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<(), Error> {
+    fs::create_dir_all(&to)?;
+
+    for entry in fs::read_dir(from)? {
+        let entry = entry?;
+        let target = to.as_ref().join(entry.file_name());
+
+        if entry.file_type()?.is_dir() {
+            copy_dir_all(entry.path(), target)?;
+        } else if !target.exists() {
+            fs::copy(entry.path(), target)?;
+        }
+    }
+
+    Ok(())
+}
+
+const CARGO_TOML: &'static str = r#"
+[package]
+name = "iceberg"
+version = "0.0.0"
+edition = "2021"
+publish = false
+
+[dependencies.iced]
+git = "https://github.com/iced-rs/iced.git"
+{{ GIT_REFERENCE }}
+features = ["webgl", "web-colors"]
+"#;
+
+const EMBEDDING: &'static str = r#"
+<script type="module" id="iceberg-script-{{ HASH }}">
+  import init from './.icebergs/{{ HASH }}/iceberg.js';
+
+  let me = document.getElementById('iceberg-script-{{ HASH }}');
+
+  // TODO: Find a more reliable way to find the code block
+  let code = me.previousSibling.previousSibling;
+  let buttons = code.querySelector('.buttons');
+  let play = document.createElement('button');
+
+  async function run() {
+    play.remove();
+
+    let example = document.createElement('div');
+    example.style.height = "300px";
+
+    let iced = document.createElement('div');
+    iced.id = 'iced';
+
+    example.append(iced);
+    code.append(example);
+
+    await init();
+  }
+
+  play.title = 'Run example';
+  play.onclick = run;
+
+  play.classList.add('fa');
+  play.classList.add('fa-play');
+
+  buttons.prepend(play);
+</script>
+"#;
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+mod compiler;
+
+use compiler::Compiler;
+
 use mdbook::book::{Book, BookItem, Chapter};
 use mdbook::errors::Error;
 use mdbook::preprocess::PreprocessorContext;
@@ -5,7 +9,6 @@ use semver::{Version, VersionReq};
 
 use std::collections::BTreeSet;
 use std::fs;
-use std::path::{Path, PathBuf};
 
 pub fn is_supported(renderer: &str) -> bool {
     renderer == "html"
@@ -24,75 +27,38 @@ pub fn run(mut book: Book, context: &PreprocessorContext) -> Result<Book, Error>
         )));
     }
 
-    let revision = context
+    let config = context
         .config
         .get_preprocessor("iced")
-        .and_then(|table| table.get("rev"))
         .ok_or(Error::msg("mdbook-iced configuration not found"))?;
 
-    let revision = revision
-        .as_str()
-        .ok_or(Error::msg("`rev` field should be a string"))?;
+    let reference = compiler::Reference::parse(config)?;
+    let compiler = Compiler::set_up(&context.root, reference)?;
 
-    let crate_ = set_up_build_crate(&context.root, revision)?;
-    let mut hashes = BTreeSet::new();
+    let mut icebergs = BTreeSet::new();
 
     for section in &mut book.sections {
         if let BookItem::Chapter(chapter) = section {
-            let (content, new_hashes) = process_chapter(&crate_, chapter)?;
+            let (content, new_icebergs) = process_chapter(&compiler, chapter)?;
 
             chapter.content = content;
-            hashes.extend(new_hashes);
+            icebergs.extend(new_icebergs);
         }
     }
 
-    let output_dir = context.root.join("src").join("icebergs");
+    let target = context.root.join("src").join(".icebergs");
+    fs::create_dir_all(&target)?;
 
-    fs::create_dir_all(&output_dir)?;
-
-    clean_output_dir(&crate_.output, &hashes)?;
-    clean_output_dir(&output_dir, &hashes)?;
-
-    copy_dir_all(&crate_.output, output_dir)?;
+    compiler.retain(&icebergs)?;
+    compiler.release(&icebergs, target)?;
 
     Ok(book)
 }
 
-struct Crate {
-    root: PathBuf,
-    src: PathBuf,
-    output: PathBuf,
-    hash: Hash,
-}
-
-fn set_up_build_crate(root: &Path, revision: &str) -> Result<Crate, Error> {
-    let build_dir = root.to_path_buf().join("icebergs");
-
-    if !build_dir.exists() {
-        fs::create_dir(&build_dir)?;
-    }
-
-    let cargo_toml = build_dir.join("Cargo.toml");
-    let contents = CARGO_TOML.replace("{{ REV }}", revision);
-
-    fs::write(cargo_toml, contents.trim_start())?;
-
-    let src_dir = build_dir.join("src");
-    if !src_dir.exists() {
-        fs::create_dir(&src_dir)?;
-    }
-
-    let output = build_dir.join("output");
-
-    Ok(Crate {
-        root: build_dir,
-        src: src_dir,
-        output,
-        hash: Hash::compute(contents.trim_start()),
-    })
-}
-
-fn process_chapter(crate_: &Crate, chapter: &Chapter) -> Result<(String, BTreeSet<Hash>), Error> {
+fn process_chapter(
+    compiler: &Compiler,
+    chapter: &Chapter,
+) -> Result<(String, BTreeSet<compiler::Iceberg>), Error> {
     use itertools::Itertools;
     use pulldown_cmark::{CodeBlockKind, Event, Parser, Tag, TagEnd};
     use pulldown_cmark_to_cmark::cmark;
@@ -118,21 +84,22 @@ fn process_chapter(crate_: &Crate, chapter: &Chapter) -> Result<(String, BTreeSe
         _ => in_iced_code,
     });
 
-    let mut hashes = BTreeSet::new();
+    let mut icebergs = BTreeSet::new();
 
     let output = groups.into_iter().flat_map(|(is_iced_code, group)| {
         if is_iced_code {
             let parts: Vec<_> = group.collect();
 
             if let Some(Event::Text(code)) = parts.get(1) {
-                match compile(crate_, code) {
-                    Ok(hash) => {
-                        hashes.insert(hash.clone());
+                match compiler.compile(code) {
+                    Ok(iceberg) => {
+                        let embedding = iceberg.embed();
+                        icebergs.insert(iceberg);
 
                         Box::new(
                             parts
                                 .into_iter()
-                                .chain(std::iter::once(Event::InlineHtml(embed(hash).into()))),
+                                .chain(std::iter::once(Event::InlineHtml(embedding.into()))),
                         )
                     }
                     Err(_) => Box::new(parts.into_iter()) as Box<dyn Iterator<Item = Event>>,
@@ -148,154 +115,5 @@ fn process_chapter(crate_: &Crate, chapter: &Chapter) -> Result<(String, BTreeSe
     let mut content = String::with_capacity(chapter.content.len());
     let _ = cmark(output, &mut content)?;
 
-    Ok((content, hashes))
+    Ok((content, icebergs))
 }
-
-fn compile(crate_: &Crate, code: &str) -> Result<Hash, Error> {
-    use itertools::Itertools;
-    use std::process;
-
-    let code = code
-        .lines()
-        .map(|line| line.strip_prefix("# ").unwrap_or(line))
-        .join("\n");
-
-    let hash = Hash::compute(&format!("{}{}", code, crate_.hash.as_str()));
-    let output_dir = crate_.output.join(hash.as_str());
-
-    if output_dir.exists() {
-        return Ok(hash);
-    }
-
-    fs::write(crate_.src.join("main.rs"), code)?;
-
-    let compilation = process::Command::new("cargo")
-        .args(["build", "--release", "--target", "wasm32-unknown-unknown"])
-        .env("RUSTFLAGS", "")
-        .current_dir(&crate_.root)
-        .stdout(process::Stdio::piped())
-        .spawn()?;
-
-    std::io::copy(
-        &mut std::io::BufReader::new(compilation.stdout.expect("Open compilation output")),
-        &mut std::io::stderr(),
-    )?;
-
-    process::Command::new("wasm-bindgen")
-        .args([
-            "--target",
-            "web",
-            "--no-typescript",
-            "--out-dir",
-            output_dir.to_str().expect("valid path"),
-            "target/wasm32-unknown-unknown/release/iceberg.wasm",
-        ])
-        .current_dir(&crate_.root)
-        .spawn()?
-        .wait()?;
-
-    Ok(hash)
-}
-
-fn clean_output_dir(dir: &Path, hashes: &BTreeSet<Hash>) -> Result<(), Error> {
-    let deleted = fs::read_dir(dir)?
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| !hashes.iter().any(|hash| hash.as_str() == entry.file_name()));
-
-    for entry in deleted {
-        fs::remove_dir_all(entry.path())?;
-    }
-
-    Ok(())
-}
-
-fn embed(hash: Hash) -> String {
-    SCRIPT.replace("{{ HASH }}", hash.as_str()).to_string()
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-struct Hash(String);
-
-impl Hash {
-    fn compute(code: &str) -> Self {
-        use itertools::Itertools;
-        use sha2::{Digest, Sha256};
-
-        Self(
-            Sha256::digest(code)
-                .into_iter()
-                .map(|byte| format!("{byte:x}"))
-                .join(""),
-        )
-    }
-
-    fn as_str(&self) -> &str {
-        &self.0
-    }
-}
-
-fn copy_dir_all(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<(), Error> {
-    fs::create_dir_all(&to)?;
-
-    for entry in fs::read_dir(from)? {
-        let entry = entry?;
-        let target = to.as_ref().join(entry.file_name());
-
-        if entry.file_type()?.is_dir() {
-            copy_dir_all(entry.path(), target)?;
-        } else if !target.exists() {
-            fs::copy(entry.path(), target)?;
-        }
-    }
-
-    Ok(())
-}
-
-const CARGO_TOML: &'static str = r#"
-[package]
-name = "iceberg"
-version = "0.0.0"
-edition = "2021"
-publish = false
-
-[dependencies.iced]
-git = "https://github.com/iced-rs/iced.git"
-rev = "{{ REV }}"
-features = ["webgl"]
-"#;
-
-const SCRIPT: &'static str = r#"
-<script type="module" id="iceberg-script-{{ HASH }}">
-  import init from './icebergs/{{ HASH }}/iceberg.js';
-
-  let me = document.getElementById('iceberg-script-{{ HASH }}');
-
-  // TODO: Find a more reliable way to find the code block
-  let code = me.previousSibling.previousSibling;
-  let buttons = code.querySelector('.buttons');
-  let play = document.createElement('button');
-
-  async function run() {
-    play.remove();
-
-    let example = document.createElement('div');
-    example.style.height = "300px";
-
-    let iced = document.createElement('div');
-    iced.id = 'iced';
-
-    example.append(iced);
-    code.append(example);
-
-    await init();
-  }
-
-  play.title = 'Run example';
-  play.onclick = run;
-
-  play.classList.add('fa');
-  play.classList.add('fa-play');
-
-  buttons.prepend(play);
-</script>
-"#;
